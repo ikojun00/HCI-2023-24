00f822cc53cd4a282a7b6822d6eeddcfb5c458af
ikojun00 <ikojun00@fesb.hr>
2023-10-14 19:05:07 +0200

    hamburger menu

--- semester-project/src/app/components/Navbar.tsx
+++ semester-project/src/app/components/Navbar.tsx
@@ -1,39 +1,107 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const currentRoute = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 640) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-            <div className="flex justify-between items-center w-full">
-                <Link href="/">
-                    <h1 className="text-xl font-bold">
-                    📖 BookVoyage
-                    </h1>
-                </Link>
-                <div className="flex justify-between gap-8">
-                    <Link href="/discover">
-                        <h2 className="text-l font-semibold hover:underline">
-                            Discover
-                        </h2>
-                    </Link>
-                    <Link href="/library">
-                        <h2 className="text-l font-semibold hover:underline">
-                            Library
-                        </h2>
-                    </Link>
-                    <Link href="/about">
-                        <h2 className="text-l font-semibold hover:underline">
-                            About
-                        </h2>
-                    </Link>
-                    <Link href="/signin">
-                        <h2 className="text-l font-semibold hover:underline">
-                            Sign In
-                        </h2>
-                    </Link>
-                </div>
-            </div>
+      <div className="flex justify-between h-16">
+        <div className="flex justify-between items-center w-full">
+          <Link href="/">
+            <h1 className="text-xl font-bold">
+              📖 BookVoyage
+            </h1>
+          </Link>
+          <div className={`${isOpen ? "flex flex-col items-end gap-0 mt-24" : ""}`}>
+          {isOpen ? (
+            <button
+              className="sm:hidden block focus:outline-none"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          ) : (
+            <button
+              className="sm:hidden block focus:outline-none"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          )}
+          <div className={`sm:flex justify-between gap-8 ${isOpen ? 'flex flex-col items-end gap-0' : 'hidden'}`}>
+            <Link href="/discover">
+              <h2 className={`text-l font-semibold hover:underline ${currentRoute === "/discover" ? "text-green-500 underline" : ""}`}>
+                Discover
+              </h2>
+            </Link>
+            <Link href="/library">
+              <h2 className={`text-l font-semibold hover:underline ${currentRoute === "/library" ? "text-green-500 underline" : ""}`}>
+                Library
+              </h2>
+            </Link>
+            <Link href="/about">
+              <h2 className={`text-l font-semibold hover:underline ${currentRoute === "/about" ? "text-green-500 underline" : ""}`}>
+                About
+              </h2>
+            </Link>
+            <Link href="/signin">
+              <h2 className={`text-l font-semibold hover:underline ${currentRoute === "/signin" ? "text-green-500 underline" : ""}`}>
+                Sign In
+              </h2>
+            </Link>
+          </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- semester-project/src/app/page.tsx
+++ semester-project/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="bg-fixed bg-center bg-cover custom-img h-screen text-white">
       <Navbar/>
       <div className="flex flex-col items-center gap-20 pt-40">
-        <h1 className="text-6xl font-bold text-center">
+        <h1 className="text-4xl sm:text-6xl font-bold text-center">
           Connecting Bookworms Worldwide.
         </h1>
         <button className="bg-green-600 rounded-3xl hover:bg-green-700">
