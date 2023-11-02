4cc261d7470070b2851bcfa25b2f91c438105e5a
ikojun00 <ikojun00@fesb.hr>
2023-11-03 00:32:43 +0100

    handling dropdown

--- semester-project/src/components/Navbar.tsx
+++ semester-project/src/components/Navbar.tsx
@@ -3,16 +3,28 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import Dropdown from "./Dropdown";
+import Community from "@/views/dropdown/Community";
+import Discover from "@/views/dropdown/Discover";
+import ReadingLog from "@/views/dropdown/ReadingLog";
 
 export default function Navbar() {
   const currentRoute = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const handleDropdownClick = (index: any) => {
+    if (activeDropdown === index) {
+      setActiveDropdown(null);
+    } else {
+      setActiveDropdown(index);
+    }
+  };
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth > 640) {
+      if (window.innerWidth > 768) {
         setIsOpen(false);
+        setActiveDropdown(null);
       }
     };
 
@@ -23,106 +35,70 @@ export default function Navbar() {
     };
   }, []);
 
-  const toggleMenu = () => {
+  const toggleHamburgerMenu = () => {
     setIsOpen(!isOpen);
+    setActiveDropdown(null);
   };
 
+  const dropdowns = [
+    { title: "Discover", component: <Discover /> },
+    { title: "Reading Log", component: <ReadingLog /> },
+    { title: "Community", component: <Community /> },
+  ];
+
+  const hamburgerIcon = (
+    <button
+      className="md:hidden block focus:outline-none"
+      onClick={toggleHamburgerMenu}
+    >
+      <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+        />
+      </svg>
+    </button>
+  );
+
   return (
-    <div className="flex justify-between h-16">
-      <div className="flex justify-between items-center w-full">
-        <Link href="/">
-          <h1 className="text-xl font-bold">📖 BookVoyage</h1>
-        </Link>
-        <div>
-          {isOpen ? (
-            <button
-              className="sm:hidden block focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          ) : (
-            <button
-              className="sm:hidden block focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          )}
-          <div
-            className={`sm:flex justify-between ${
-              isOpen ? "" : "hidden gap-8"
-            }`}
-          >
-            {isOpen ? (
-              <Dropdown heading="" />
-            ) : (
-              <>
-                <Link href="/discover">
-                  <h2
-                    className={`text-l font-semibold hover:underline ${
-                      currentRoute === "/discover"
-                        ? "text-green-500 underline"
-                        : ""
-                    }`}
-                  >
-                    Discover
-                  </h2>
-                </Link>
-                <Dropdown heading="Community" />
-                <Link href="/about">
-                  <h2
-                    className={`text-l font-semibold hover:underline ${
-                      currentRoute === "/about"
-                        ? "text-green-500 underline"
-                        : ""
-                    }`}
-                  >
-                    About
-                  </h2>
-                </Link>
-                <Link href="/signin">
-                  <h2
-                    className={`text-l font-semibold hover:underline ${
-                      currentRoute === "/signin"
-                        ? "text-green-500 underline"
-                        : ""
-                    }`}
-                  >
-                    Sign In
-                  </h2>
-                </Link>
-              </>
-            )}
+    <div className="flex justify-between items-center p-4">
+      {hamburgerIcon}
+      <Link href="/">
+        <h1 className="text-xl font-bold">📖 BookVoyage</h1>
+      </Link>
+      <div
+        className={`md:flex ${
+          isOpen
+            ? "absolute text-center top-14 right-0 w-full bg-white pt-8"
+            : "hidden gap-8"
+        }`}
+      >
+        {dropdowns.map((item, index) => (
+          <div className="mb-8 md:mb-0" key={index} onClick={() => handleDropdownClick(index)}>
+            <h2 className="text-base font-semibold hover:underline cursor-pointer">
+              {item.title}
+            </h2>
+            {activeDropdown === index && <>{item.component}</>}
           </div>
-        </div>
+        ))}
       </div>
+      <Link href="/signin">
+        <h2
+          className={`text-l font-semibold hover:underline ${
+            currentRoute === "/signin" ? "text-green-500 underline" : ""
+          }`}
+        >
+          Sign In
+        </h2>
+      </Link>
     </div>
   );
 }
