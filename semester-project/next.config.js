3dd99d62d5d4abffdeb6b24ca47915ffa48ba2cb
ikojun00 <ikojun00@fesb.hr>
2023-11-05 14:59:54 +0100

    config sending data between pages

--- semester-project/next.config.js
+++ semester-project/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
+const nextConfig = {
+  images: {
     domains: ["books.google.com"],
-  },}
+  },
+  env: {
+    API_KEY: process.env.API_KEY
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
