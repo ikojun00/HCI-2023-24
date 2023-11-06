fef2cec95ad6b9db3e836639b36bc812efea47be
ikojun00 <ikojun00@fesb.hr>
2023-11-06 21:06:51 +0100

    changing layout and logic of searching books

--- semester-project/src/app/discover/[bookId]/page.tsx
+++ semester-project/src/app/discover/[bookId]/page.tsx
@@ -1,15 +1,49 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
 interface Params {
-  bookId: string;
+  title: string;
+  authors?: string[];
+  image: string;
+  description: string;
 }
 
-export interface BookParams {
-  params: Params;
+interface BookParams {
+  searchParams: Params;
 }
 
-export default function BlogPost({ params }: BookParams) {
+export default function BlogPost({ searchParams }: BookParams) {
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-3xl font-bold p-10">Book #{params.bookId}</h1>
+    <main className="flex flex-col">
+      <div className="border-b-2">
+        <div className="flex flex-col max-w-screen-xl mx-auto px-2 sm:px-4 lg:px-6">
+          <Navbar />
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="flex flex-col justify-start max-w-screen-xl mx-auto px-2 sm:px-4 lg:px-6">
+        <div className="flex flex-row">
+          <Image
+            src={searchParams.image}
+            alt="Cover"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "200px", height: "auto" }}
+          />
+          <div className="flex flex-col pl-8 gap-4">
+            <h1 className="text-3xl font-bold">{searchParams.title}</h1>
+            <div className="flex flex-row gap-1 items-center">
+              <h1>By:</h1>
+              <h1 className="text-xl font-bold">{searchParams.authors}</h1>
+            </div>
+          </div>
+        </div>
+        <br />
+        <p>{searchParams.description}</p>
+      </div>
     </main>
   );
 }
--- semester-project/src/services/GoogleBooksService.ts
+++ semester-project/src/services/GoogleBooksService.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-export default async function GoogleBooksService(searchTerm: string) {
-    try {
-        const response = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=4`
-        );
-        const bookData = response.data.items;
-        return bookData;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+export default async function GoogleBooksService(
+  searchTerm: string,
+  filter: string,
+  category: string
+) {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${filter}${searchTerm}${category}&printType=books&key=${process.env.API_KEY}`
+    );
+    const bookData = response.data.items;
+    return bookData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
