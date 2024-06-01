376508086b9a04be9cd2e0d590b2f2afbdc1ad3d
igovor01 <igovor01@fesb.hr>
2024-06-01 02:06:35 +0200

    no shringing - responsiveness

--- semester-project/src/views/dashboard/DashboardBookItem.tsx
+++ semester-project/src/views/dashboard/DashboardBookItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function DashboardBookItem({ book }: Props) {
   return (
-    <div className="w-24 sm:w-32 md:w-40 flex flex-col gap-2">
+    <div className="w-24 sm:w-32 md:w-40 flex flex-col gap-2 shrink-0">
       <DashboardBookCover
         coverUrl={book ? book.cover.url : "/no-book-in-category.png"}
       />
