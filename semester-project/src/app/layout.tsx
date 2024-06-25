1e527df59495e79cb2586217d8e97c867c04d7e9
igovor01 <igovor01@fesb.hr>
2024-06-25 22:44:30 +0200

    adding favicon

--- semester-project/src/app/layout.tsx
+++ semester-project/src/app/layout.tsx
@@ -5,13 +5,20 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/views/footer/Footer";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Book Voyage",
   description: "Bookworm's favorite place to be",
+  icons: {
+    icon: [
+      { rel: "icon", url: "/favicon.ico" },
+      { rel: "icon", url: "/favicon-32x32.ico", sizes: "32x32" },
+      { rel: "icon", url: "/favicon-64x64.ico", sizes: "64x64" },
+    ],
+  },
 };
 
 export default function RootLayout({
